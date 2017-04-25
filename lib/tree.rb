003dbd72dca0c73805a77c166c7c53e00408a21c
Shirong Foo <dee.sr.foo@gmail.com>
2017-04-25 23:02:48 +0800

    passed tests

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,12 @@
 class Tree
-  def initialize(type, height, age)
+  attr_accessor :type, :height, :age
+  def initialize(type = 'oak', height = 65, age = 2)
+    @type = type
+    @height = height
+    @age = age
   end
 
   def grow_tree(water_amount)
+    @height += water_amount * 0.1
   end
 end
